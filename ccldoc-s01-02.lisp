21a2d6200840d2a1619cf8ab43d44855f9e03a84
Kamil Ziemian <kziemianfvt@gmail.com>
2017-12-11 22:29:00 +0100

    GENSYM

--- ccldoc-s01-02.lisp
+++ ccldoc-s01-02.lisp
@@ -2,6 +2,7 @@
 ;; Tworzenie strony "Function GENSYM"
 
 
+;; ##########
 ;; Konfiguracja :CCLDOC
 (progn
   (load "home:quicklisp;setup")
@@ -13,22 +14,63 @@
 
 
 
+
+
 ;; Tworzenie pliku HTML
 
 (progn
-  (defparameter *plik-numer* "01")
+  (defparameter *plik-numer* "03")
   (defparameter *plik-nazwa* (concatenate 'string
 					  +sciezka+
-					  "przyklad-s01-"
+					  "example-s01-"
 					  *plik-numer*))
-  (defparameter *d* (let ((*package* *package*))
-		      (ccldoc:load-document (concatenate 'string
-							 *plik-nazwa*
-							 ".ccldoc"))))
+  (defparameter *document* (let ((*package* *package*))
+			     (ccldoc::load-document (concatenate 'string
+								 *plik-nazwa*
+								 ".ccldoc"))))
 
-  (ccldoc::output-html *d* (concatenate 'string
-					*plik-nazwa*
-					".html")
-		       :stylesheet "ccl.css"))
+  (ccldoc::output-html *document* (concatenate 'string
+					       *plik-nazwa*
+					       ".html")
+		       :stylesheet "ccl.css" ; Tę linię można zakomentować
+		       ;; i wciąż będzie działał, tylko nie użyje tego pliku
+		       ;; CSS. Zapewne nie użyje żadnego.
+		       ))
+
+(probe-file (concatenate 'string *plik-nazwa* ".ccldoc"))
+
+;; W Overview jest informacja, że operatory są zamieniane na keywordsy
+;; w czasie procesowania pliku i wedle moich testów równie dobrze działa
+;; ccldoc::load-document jak ccldoc:load-document
+;; i tak samo
+;; ccldoc::output-html jak ccldoc:output-html
+;; Ja przyjąłem, że lepiej jest pisać ccldoc::load-document
+;; i ccldoc::output-html.
+
+;; Kod na stronie https://trac.clozure.com/ccldoc/ używa
+;; ccldoc:load-document i ccldoc::output-html
+
+
+
+
+
+;; ##########
+;; Tworzenie strony GENSYM
+
+(progn
+  (defvar *plik-ccldoc* (concatenate 'string
+				     +sciezka+
+				     "gensym.ccldoc"))
+  (defvar *plik-html* (concatenate 'string
+				   +sciezka+
+				   "gensym.html"))
+  (defparameter *document* (let ((*package* *package*))
+			     (ccldoc::load-document *plik-ccldoc*)))
+
+  (ccldoc::output-html *document* *plik-html*
+		       :stylesheet "ccl.css" ; Tę linię można zakomentować
+		       ;; i wciąż będzie działał, tylko nie użyje tego pliku
+		       ;; CSS. Zapewne nie użyje żadnego.
+		       ))
 
 (probe-file (concatenate 'string *plik-nazwa* ".ccldoc"))
